56734478303902fae7183dc4fda5c6395a9b5ba8
anitapm03 <apereiramolinero@gmail.com>
2024-05-23 09:24:22 +0200

    Add project files.

---
+++ ConsoleAWSSecrets/Helpers/HelperSecretManager.cs
@@ -0,0 +1,35 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAWSSecrets.Helpers
+{
+    internal class HelperSecretManager
+    {
+        public static async Task<string> GetSecretAsync()
+        {
+            string secretName = "datasecrets";
+            string region = "us-east-1";
+
+            IAmazonSecretsManager client =
+                new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+            GetSecretValueRequest request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+                VersionStage = "AWSCURRENT"
+            };
+
+            GetSecretValueResponse response;
+            response = await client.GetSecretValueAsync(request);
+            string secret = response.SecretString;
+            return secret;
+        }
+
+    }
+}
---
+++ ConsoleAWSSecrets/Models/KeysModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAWSSecrets.Models
+{
+    internal class KeysModel
+    {
+        public string Mysql { get; set; }
+        public string Bucket { get; set; }
+        public string Api { get; set; }
+
+    }
+}
